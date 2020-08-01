0ac91480479717f48cb1082f9a3fd4cd9d12e0d5
Astralite <binarysproduct@gmail.com>
2020-08-02 01:02:36 +0800

    Day 3 - Git and Github

---
+++ Day 3/src/main.java
@@ -0,0 +1,13 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		if (true) {
+			System.out.println("I can use git. Yay");			
+		} else {
+			System.out.println("Oh no's");
+		}
+
+	}
+
+}
