fb1cf6fa7dc66e2a1a13ed680fd3d474535086a5
Astralite <binarysproduct@gmail.com>
2020-08-02 13:55:42 +0800

    Day 4 - Read from file

--- Day 4/src/main.java
+++ Day 4/src/main.java
@@ -1,24 +1,14 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class main {
 
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
+	public static void main(String[] args) throws FileNotFoundException {
+		Scanner in = new Scanner(new File("src/test.txt"));
 		
-		System.out.println("Please enter a size for array");
-		
-		int size = in.nextInt();
-		int[] grades = new int[size];
-		
-		for(int i = 0; i < grades.length; i++) {
-			System.out.println(i + ". Grade: ");
-			grades[i] = in.nextInt();
-		}
-		
-		in.close();
-		
-		for (int i = 0; i < grades.length; i++) {
-			System.out.println("Grade #" + i + ": " + grades[i]);
+		while(in.hasNextLine()) {
+			System.out.println("line: " + in.nextLine());
 		}
 	}
 
