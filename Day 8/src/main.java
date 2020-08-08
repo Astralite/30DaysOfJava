6080f98b3ce6688b45d6a96630381c65143b74f4
Astralite <binarysproduct@gmail.com>
2020-08-08 14:42:05 +0800

    Day 8 - Experiment with linked lists

--- Day 8/src/main.java
+++ Day 8/src/main.java
@@ -1,5 +1,15 @@
+import java.util.Iterator;
+import java.util.LinkedList;
 
 public class main {
+	
+	static class Explorer {
+		LinkedList<int[]> path = new LinkedList<int[]>();
+		
+		public Explorer(int[] startPos) {
+			path.push(startPos);
+		}
+	}
 
 	public static void main(String[] args) {
 		
@@ -8,12 +18,23 @@ public class main {
 				{1, 1, 1, 1},
 				{0, 0, 2, 1}
 		};
-
-		for(int[] i: maze) {
-			for(int j: i) {
-				System.out.println(j);
-			}
+		
+		Explorer mouse = new Explorer(new int[] {0, 1});
+		
+		mouse.path.push(new int[] {1, 1});
+		mouse.path.push(new int[] {1, 2});
+		mouse.path.push(new int[] {2, 2});
+		
+		while(!mouse.path.isEmpty()) {
+			int[] location = mouse.path.pop();
+			System.out.println(location[0] + " " + location[1]);
 		}
+		
+//		for(int[] i: maze) {
+//			for(int j: i) {
+//				System.out.println(j);
+//			}
+//		}
 		
 	}
 
